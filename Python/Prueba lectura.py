264e270b240dd32fe70c02e4a1a0b08bb393faeb
elestrategaactual <51063748+elestrategaactual@users.noreply.github.com>
2022-10-31 09:03:03 -0500

    codigo de prueba de lectura

--- Python/Prueba lectura.py
+++ Python/Prueba lectura.py
@@ -5,5 +5,29 @@ Created on Mon Oct 31 08:43:38 2022
 @author: ivanm
 """
 
-import cvs
+
 import numpy as np
+
+raw_data = open('qDots.csv')
+qDots = np.loadtxt(raw_data, delimiter=",",skiprows=1)
+
+raw_data = open('qCircle.csv')
+qCircle = np.loadtxt(raw_data, delimiter=",",skiprows=1)
+
+raw_data = open('qDejado.csv')
+qDejado = np.loadtxt(raw_data, delimiter=",",skiprows=1)
+
+raw_data = open('qLetter.csv')
+qLetter = np.loadtxt(raw_data, delimiter=",",skiprows=1)
+
+raw_data = open('qPar.csv')
+qPar = np.loadtxt(raw_data, delimiter=",",skiprows=1)
+
+raw_data = open('qRecolecccion.csv')
+qRecoleccion = np.loadtxt(raw_data, delimiter=",",skiprows=1)
+
+raw_data = open('qTriangle.csv')
+qTriangle = np.loadtxt(raw_data, delimiter=",",skiprows=1)
+
+print(qDots.shape)
+print(qDots)
